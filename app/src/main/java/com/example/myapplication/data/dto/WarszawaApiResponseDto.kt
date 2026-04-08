3a25323d52a0d440dcedd91021bb11616d427096
tymcza <tymon.czarnecki1@gmail.com>
2026-04-08 10:09:54 +0200

    feat: add dto for warszawa api

---
+++ app/src/main/java/com/example/myapplication/data/dto/WarszawaApiResponseDto.kt
@@ -0,0 +1,5 @@
+package com.example.myapplication.data.dto
+
+data class WarszawaApiResponseDto(
+    val result: List<WarszawaApiRouteDto>
+)
---
+++ app/src/main/java/com/example/myapplication/data/dto/WarszawaApiRouteDto.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication.data.dto
+
+data class WarszawaApiRouteDto(
+    val length: String,
+    val description: String,
+    val title: String
+)
