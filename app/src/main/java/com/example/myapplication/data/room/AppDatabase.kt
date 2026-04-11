46c7856394f8d0bfb1d264fec1d170e42c756262
tymcza <tymon.czarnecki1@gmail.com>
2026-04-11 19:39:35 +0200

    style: delete unused import

--- app/src/main/java/com/example/myapplication/data/room/AppDatabase.kt
+++ app/src/main/java/com/example/myapplication/data/room/AppDatabase.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.myapplication.data.room.RoutesDao
 import com.example.myapplication.data.types.entities.RouteRoom
 
 @Database(entities = [RouteRoom::class], version = 1)
